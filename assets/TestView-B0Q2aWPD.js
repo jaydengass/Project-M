1153421ce26e008e5d259465c9ce567992ec6326
Developer <user@example.com>
2026-04-14 17:07:37 -0500

    Deploy site

---
+++ assets/TestView-B0Q2aWPD.js
@@ -0,0 +1,2 @@
+import{r as e,j as s}from"./vendor-CmOI1LkD.js";import{B as o}from"./index-DNdSkafa.js";function i(){const[r,t]=e.useState(!1);if(r)throw new Error("I crashed");return s.jsx(o,{onClick:()=>t(!0),children:"Crash me!"})}export{i as default};
+//# sourceMappingURL=TestView-B0Q2aWPD.js.map
